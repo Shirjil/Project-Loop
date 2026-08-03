044d1db498691faca92a586e9618282d1ba8c8c9
Shirjil Kamran <mshirjilkamran@gmail.com>
2026-08-03 20:01:38 +0530

    Fix production API URL

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/users";
+const API_URL = "https://project-loop-backend-1.onrender.com/api/users";
+
 
 export const loginUser = (data) => {
     return axios.post(`${API_URL}/login`, data);
